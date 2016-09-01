11ab0c23e7d905943476531a0896200ce345ad08
Triggersfun <garfunkelnator@gmail.com>
2016-09-01 15:16:58 -0400

    Update JPGWalker.py

--- JPGWalker.py
+++ JPGWalker.py
@@ -1,13 +1,14 @@
+import os  # imports os components
 all_jpgs = [] # define the list of all_jpgs
 
 suffix = (".jpg",".jpeg")  # define a tuple of image types
 def all_pictures(): # define a function to recursively search through Pictures folder on C drive
-    import os  # imports os components
     for root, dirs, files in os.walk('C:\\Users\\Sam\\Pictures'):  # recursively searches for jpg images
              for item in files:
-                 if item.lower().endswith(suffix):  # searches for suffix through jpgs
-                    all_jpgs.append(os.path.join(root, item):  # appends image location to list
+                 if item.lower().endswith(suffix)== True:  # searches for suffix through jpgs
+                    all_jpgs.append(os.path.join(root, item))  # appends image location to list
 
 all_pictures()
+
 print(all_jpgs)  # prints list of image locations
 print(len(all_jpgs))  # prints number of images found
