dbdea1bd46106d4dd8239993a1f9380f7fe7c9e9
Triggersfun <samgriffith3@gmail.com>
2016-09-02 13:58:58 -0400

    Updated 9/2

--- CSV to JSON.py
+++ CSV to JSON.py
@@ -1,14 +1,48 @@
-import csv     # imports the csv module
+import sys, getopt
+import csv
 import json
-
-csv_file = open('C:\\Users\\Sam\\Downloads\\SalesJan2009.csv')
-read_csv = csv.reader(csv_file)
-csv_data = list(read_csv)
-
-print(csv_data)
+import pprint
 
 
-with open('csv_data', 'w') as outfile:
-    json.dump(csv_data, outfile)
+#Get Command Line Arguments
+def main(argv):
+    input_file = 'C:\\Users\\Sam\\Downloads\\SalesJan2009.csv'
+    output_file = 'C:\\Users\\Sam\\Documents\\Scoury.json'
+    format = 'pprint'
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:f:",["ifile=","ofile=","format="])
+    except getopt.GetoptError:
+        print ('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('csv_json.py -i <path to inputfile> -o <path to outputfile> -f <dump/pretty>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_file = arg
+        elif opt in ("-o", "--ofile"):
+            output_file = arg
+        elif opt in ("-f", "--format"):
+            format = arg
+    read_csv(input_file, output_file, format)
 
-print(outfile)
+#Read CSV File
+def read_csv(file, json_file, format):
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        title = reader.fieldnames
+        for row in reader:
+            csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
+        write_json(csv_rows, json_file, format)
+
+#Convert csv data into json and write it
+def write_json(data, json_file, format):
+    with open(json_file, "w") as f:
+        if format == "pretty":
+            f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
+        else:
+            f.write(json.dumps(data))
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
