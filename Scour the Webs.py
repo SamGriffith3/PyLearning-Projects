73aae153e6c3668bd966e610fdc45e100d22e5fc
Triggersfun <samgriffith3@gmail.com>
2016-09-02 14:00:26 -0400

    Updated 9/2

--- Scour the Webs.py
+++ Scour the Webs.py
@@ -1,13 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import smtplib
-from email.mime.text import MIMEText
-
-import json
-
 
 r = requests.get('http://cdgriffith.com')  #
-
 
 soup = BeautifulSoup(r.text, 'html.parser')
 for link in soup.find_all('a'):
