ca4b9270e8e7905e0a94b66732d025a0b4b88ab4
Triggersfun <samgriffith3@gmail.com>
2016-09-02 22:57:08 -0400

    Updated 9/2

--- projects/scour_the_webs.py
+++ projects/scour_the_webs.py
@@ -1,19 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://cdgriffith.com')
 
-soup = BeautifulSoup(r.text, 'html.parser')
-links = []
-for link in soup.find_all('a'):
-    print(link.get('href'))
+def scour_the_webs(web_page, output="output.txt"):  # set web_page = (your web page), output.txt set to default
 
-    links.append(link.get('href'))
+    r = requests.get(web_page)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = []
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+        if link.get('href').startswith("http"):
+            links.append(link.get('href'))
 
-print(links)
-with open('Outputttt.txt', "w") as f:
-    for l in links:
-        f.write(l)
+    print(links)
+    with open(output, "w") as f:
+        for l in links:
+            f.write(l + '\n')
 
 
 
