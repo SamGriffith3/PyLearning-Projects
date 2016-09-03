94fecf140042b2f71a413651c2e2170be1773b9e
Triggersfun <samgriffith3@gmail.com>
2016-09-02 22:47:37 -0400

    Updated 9/2

--- projects/scour_the_webs.py
+++ projects/scour_the_webs.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 r = requests.get('http://cdgriffith.com')
 
 soup = BeautifulSoup(r.text, 'html.parser')
+links = []
 for link in soup.find_all('a'):
     print(link.get('href'))
-    links = []
+
     links.append(link.get('href'))
-    print(links)
-with open('Output.txt', "w") as f:
+
+print(links)
+with open('Outputttt.txt', "w") as f:
     for l in links:
         f.write(l)
 
