befc90176f205a0faf9cf3239685e2efad1222b0
Samgriffith3 <garfunkelnator@gmail.com>
2016-09-04 22:43:25 -0400

    Updated 9/4

--- projects/battleship.py
+++ projects/battleship.py
@@ -5,6 +5,7 @@ import pprint
 def new_matrix():
     return [[0 for _ in range(10)] for _ in range(10)]
 
+
 p1_matrix = new_matrix()
 p2_matrix = new_matrix()
 
@@ -25,18 +26,16 @@ def matrix_sum(player):  # Sums the matrix for given player
 
 # Functions
 def place_a_ship(s, player):
-
     print("Place your ", s)
     length = s['ship_len']
     vertical = True if input("vertical? (y/n): ") in ("y", "Y") else False
-    #start_at = input("Where should I start it (bottom/left)?")
+    # start_at = input("Where should I start it (bottom/left)?")
 
-    start_x = int(input("Vertical #"))
-    start_y = int(input('Horizontal #'))
+    start_y = int(input('Vertical #'))
+    start_x = int(input("Horizontal #"))
 
     assert 0 <= start_x < 10
     assert 0 <= start_y < 10
-
 
     if player == 1:
         for i in range(length):
@@ -51,34 +50,8 @@ def place_a_ship(s, player):
             else:
                 p2_matrix[start_y][start_x + i] = s['ship_len']
 
-def take_a_turn(player):
-    if player == 1:
-        print("Player 1, Your Guesses So Far")
-        pprint.pprint (p1_guessed_matrix)
-        guess_x = int(input("Sideways #"))
-        guess_y = int(input("Vertical #"))
-        if p2_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
-            p1_guessed_matrix[guess_x][guess_y] = 1
-            print("MISSED")
-        else:
-            p1_guessed_matrix[guess_x][guess_y] = 9
-            print("HIT")
-            p2_matrix[guess_x][guess_y] = 0
-    else:
-        print("Player 2, Your Guesses So Far")
-        pprint.pprint(p2_guessed_matrix)
-        guess_x = int(input("Sideways #"))
-        guess_y = int(input("Vertical #"))
-        if p1_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
-            p2_guessed_matrix[guess_x][guess_y] = 1
-            print("MISSED")
-        else:
-            p2_guessed_matrix[guess_x][guess_y] = 9
-            print("HIT")
-            p1_matrix[guess_x][guess_y] = 0
 
-
-#  This is where the game starts
+# This is where the game starts
 print("Player 1, Your Turn!")
 for ship in (aircraft_carrier, battleship, submarine, destroyer, dingy):
     place_a_ship(ship, 1)
@@ -95,20 +68,71 @@ p2_guessed_matrix = new_matrix()
 z = matrix_sum(1)
 i = matrix_sum(2)
 
-while z and i != 0:
-    print("game on")
-    take_a_turn(1)
-    take_a_turn(2)
 
-if z == 0:
-    print("Player 2 Wins!!!")
-elif i == 0:
-    print("Player 1 Wins!!!")
+def take_a_turn(player):
+    if player == 1:
+        print("Player 1, Your Guesses So Far")
+        pprint.pprint(p1_guessed_matrix)
+        guess_y = int(input("Horizontal # : "))
+        guess_x = int(input("Vertical # : "))
+
+        if p2_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
+            p1_guessed_matrix[guess_x][guess_y] = 1
+            print("MISSED")
+        else:
+            p1_guessed_matrix[guess_x][guess_y] = 9
+            print("HIT")
+            p2_matrix[guess_x][guess_y] = 0
+
+    else:
+        print("Player 2, Your Guesses So Far")
+        pprint.pprint(p2_guessed_matrix)
+        guess_y = int(input("Horizontal # : "))
+        guess_x = int(input("Vertical # : "))
+        if p1_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
+            p2_guessed_matrix[guess_x][guess_y] = 1
+            print("MISSED")
+        else:
+            p2_guessed_matrix[guess_x][guess_y] = 9
+            print("HIT")
+            p1_matrix[guess_x][guess_y] = 0
+
+
+def run_turn(player):
+    while True:
+        try:
+            take_a_turn(player)
+        except Exception:
+            print("Fat finger idiot")
+            continue
+        else:
+            break
+
+
+while i and z != 0:
+    z = matrix_sum(1)
+    i = matrix_sum(2)
+    if z and i != 0:
+        print("game on")
+        run_turn(1)
+        run_turn(2)
+        pprint.pprint(p1_matrix)
+        pprint.pprint(p2_matrix)
+
+    elif z == 0:
+        print("Player 2 Wins!!!")
+        break
+    else:
+        print("Player 1 Wins!!!")
+        break
 
 print("Player 1's Board")
 pprint.pprint(p1_matrix)
 print("Player 2's Board")
 pprint.pprint(p2_matrix)
+
+
+
 
 
 
--- projects/matrix.py
+++ projects/matrix.py
@@ -5,6 +5,7 @@ import pprint
 def new_matrix():
     return [[0 for _ in range(10)] for _ in range(10)]
 
+
 p1_matrix = new_matrix()
 p2_matrix = new_matrix()
 
@@ -25,19 +26,16 @@ def matrix_sum(player):  # Sums the matrix for given player
 
 # Functions
 def place_a_ship(s, player):
-
     print("Place your ", s)
     length = s['ship_len']
     vertical = True if input("vertical? (y/n): ") in ("y", "Y") else False
-    #start_at = input("Where should I start it (bottom/left)?")
+    # start_at = input("Where should I start it (bottom/left)?")
 
     start_y = int(input('Vertical #'))
     start_x = int(input("Horizontal #"))
 
-
     assert 0 <= start_x < 10
     assert 0 <= start_y < 10
-
 
     if player == 1:
         for i in range(length):
@@ -53,15 +51,15 @@ def place_a_ship(s, player):
                 p2_matrix[start_y][start_x + i] = s['ship_len']
 
 
-
-#  This is where the game starts
+# This is where the game starts
 print("Player 1, Your Turn!")
-
-place_a_ship(dingy, 1)
+for ship in (aircraft_carrier, battleship, submarine, destroyer, dingy):
+    place_a_ship(ship, 1)
 pprint.pprint(p1_matrix)
 
 print("Player 2, Your Turn!")
-place_a_ship(dingy, 2)
+for ship in (aircraft_carrier, battleship, submarine, destroyer, dingy):
+    place_a_ship(ship, 2)
 pprint.pprint(p2_matrix)
 
 p1_guessed_matrix = new_matrix()
@@ -70,12 +68,14 @@ p2_guessed_matrix = new_matrix()
 z = matrix_sum(1)
 i = matrix_sum(2)
 
+
 def take_a_turn(player):
     if player == 1:
         print("Player 1, Your Guesses So Far")
-        pprint.pprint (p1_guessed_matrix)
-        guess_y = int(input("Horizontal #"))
-        guess_x = int(input("Vertical #"))
+        pprint.pprint(p1_guessed_matrix)
+        guess_y = int(input("Horizontal # : "))
+        guess_x = int(input("Vertical # : "))
+
         if p2_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
             p1_guessed_matrix[guess_x][guess_y] = 1
             print("MISSED")
@@ -87,8 +87,8 @@ def take_a_turn(player):
     else:
         print("Player 2, Your Guesses So Far")
         pprint.pprint(p2_guessed_matrix)
-        guess_y = int(input("Horizontal #"))
-        guess_x = int(input("Vertical #"))
+        guess_y = int(input("Horizontal # : "))
+        guess_x = int(input("Vertical # : "))
         if p1_matrix[guess_x][guess_y] == 0:  # Hit/miss loop
             p2_guessed_matrix[guess_x][guess_y] = 1
             print("MISSED")
@@ -98,13 +98,24 @@ def take_a_turn(player):
             p1_matrix[guess_x][guess_y] = 0
 
 
+def run_turn(player):
+    while True:
+        try:
+            take_a_turn(player)
+        except Exception:
+            print("Fat finger idiot")
+            continue
+        else:
+            break
+
+
 while i and z != 0:
     z = matrix_sum(1)
     i = matrix_sum(2)
     if z and i != 0:
         print("game on")
-        take_a_turn(1)
-        take_a_turn(2)
+        run_turn(1)
+        run_turn(2)
         pprint.pprint(p1_matrix)
         pprint.pprint(p2_matrix)
 
@@ -119,12 +130,6 @@ print("Player 1's Board")
 pprint.pprint(p1_matrix)
 print("Player 2's Board")
 pprint.pprint(p2_matrix)
-
-
-
-
-
-
 
 
 
