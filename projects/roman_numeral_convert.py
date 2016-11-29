07cccdbb7fdc6919503c5b11bca8604e1f7a0d59
SamGriffith3 <garfunkelnator@gmail.com>
2016-11-28 20:06:12 -0500

    Create roman_numeral_convert.py

---
+++ projects/roman_numeral_convert.py
@@ -0,0 +1,34 @@
+
+#Roman numerals are: [i v x l c d m]
+
+def stringer (x):
+  number_string = str(x)
+  a = number_string[0]
+  b = number_string[1] 
+  c = number_string[2] 
+  d = number_string[3] 
+
+
+a_list = [ I, II, III, IV, V, VI, VII, VIII, IX]
+b_list = [ X, XX, XXX, XL, L, LX, LXX, LXXX, XC]
+c_list = [ C, CC, CCC, CD, D, DC, DCC, DCCC, CM]
+d_list = [ M, MM, MMM]
+
+
+x = int(input("Your Number(up to 3999):  "))
+
+stringer
+
+print a_list[a-1]
+print b_list[b-1]
+print c_list[c-1]
+print d_list[d-1]
+
+
+
+
+ 
+    
+    
+    
+
